e4ef547cee76b4b33e1f4ddcf26f965d9e3bc66f
Ong Ke Wei <43628709+ok-kewei@users.noreply.github.com>
2020-11-03 01:59:46 +0000

    initial commit

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/user/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/home/user/simulation_ws/devel/lib:/home/simulations/public_sim_ws/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/lib:/usr/local/lib/gazebo-7/plugins:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
+export PATH="/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/home/simulations/public_sim_ws/devel/bin:/opt/ros/kinetic/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/opt/ros/kinetic/share/euslisp/jskeus/eus//Linux64/bin:/home/user/.nvm/versions/node/v6.11.2/bin:/home/user/bin:/home/user/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+export PWD="/home/user/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/user/catkin_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/user/catkin_ws/src:$ROS_PACKAGE_PATH"
---
+++ catkin_ws/src/get_map_data/src/call_map_service.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+from nav_msgs.srv import GetMap, GetMapRequest
+
+
+rospy.init_node('service_client') # Initialise a ROS node with the name service_client
+rospy.wait_for_service('/static_map') # Wait for the service /static_map to be running
+get_map_service = rospy.ServiceProxy('/static_map', GetMap) # Create the connection to the service
+get_map = GetMapRequest() # Create an object of type GetMapRequest
+result = get_map_service(get_map) # Call the service
+print result # Print the result given by the service called
